7a8b952e3539b93dc629bf7a479e41c93898358f
Arden Collier <ardencollier1297@gmail.com>
2020-11-04 10:59:08 -0500

    can view items, add to cart and view total

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import { createStore } from 'redux';
+import reducers from './reducers/index';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider >
+    , document.getElementById('root'));
